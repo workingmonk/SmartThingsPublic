78a919b102c0d337851c6a339a7ca351ca477748
Vinay Rao <vinay@smartthings.com>
2015-12-07 16:57:58 -0800

    updating copyright notice

--- devicetypes/smartthings/zigbee-rgbw-bulb.src/zigbee-rgbw-bulb.groovy
+++ devicetypes/smartthings/zigbee-rgbw-bulb.src/zigbee-rgbw-bulb.groovy
@@ -1,9 +1,20 @@
-/*
-    Osram Flex RGBW Light Strip
-
-    Osram bulbs have a firmware issue causing it to forget its dimming level when turned off (via commands). Handling
-    that issue by using state variables
-*/
+/**
+ *  Copyright 2015 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  ZigBee RGBW Bulb
+ *
+ *  Author: SmartThings
+ *  Date: 2015-12-09
+ */
 
 metadata {
     definition (name: "ZigBee RGBW Bulb", namespace: "smartthings", author: "SmartThings") {
